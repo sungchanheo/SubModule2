6b4084884b90fed61e84823d414043c953df76d7
sungchanheo <sungchan.heo@penta-cube.com>
2021-10-13 11:29:39 +0900

    첫 커밋

---
+++ WpfApp3/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Dog : Animal, IAnimal
+    {
+        public string Bark { get; set; }
+
+        public string Kind { get; set; }
+
+        public string Color { get; set; }
+        public string Race { get; set; }
+    }
+}
